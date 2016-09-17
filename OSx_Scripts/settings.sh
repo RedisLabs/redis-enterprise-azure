f3dbb5b34dec877dd9fdac33f6b192c68004921c
Cihangir Biyikoglu <cihan@Salmon.local>
2016-09-16 17:02:22 -0700

    seeding

--- OSx_Scripts/settings.sh
+++ OSx_Scripts/settings.sh
@@ -2,7 +2,7 @@
 
 # The MIT License (MIT)
 #
-# Copyright (c) 2015 Couchbase
+# Copyright (c) 2015 Redis Labs
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -25,21 +25,21 @@
 # Script Name: settings.sh
 # Author: Cihan Biyikoglu - github:(cihanb)
 
-##couchbase settings
+##rlec settings
 #total nodes in cluster
-couchbase_total_nodes=3
-#ubuntu 14 image for couchbase server. version can be 4.0 or later
-couchbase_download="http://packages.couchbase.com/releases/4.5.0/couchbase-server-enterprise_4.5.0-ubuntu14.04_amd64.deb"
-couchbase_binary="couchbase-server-enterprise_4.5.0-ubuntu14.04_amd64.deb"
+rlec_total_nodes=3
+#ubuntu 14 image for rlec server. version can be 4.0 or later
+rlec_download="https://s3.amazonaws.com/rlec-downloads/4.3.0/redislabs-4.3.0-230-trusty-amd64.tar"
+rlec_binary="redislabs-4.3.0-230-trusty-amd64.tar"
 #TODO: change this username
-couchbase_admin_account_name="administrator"
+rlec_admin_account_name="administrator"
 #TODO: change this password
-couchbase_admin_account_password="password"
-#couchbase services on nodes
-couchbase_node_services="data,query,index"
+rlec_admin_account_password="password"
+#rlec services on nodes
+rlec_node_services="data,query,index"
 #ram configurations
-couchbase_cluster_index_ramsize=1000
-couchbase_cluster_ramsize=1000
+rlec_cluster_index_ramsize=1000
+rlec_cluster_ramsize=1000
 
 ##azure settings
 #TODO: use "azure login -u account" +  "azure account show" to get  account and subscriptionid
@@ -49,11 +49,11 @@ azure_subscription_id="00000000-0000-0000-0000-000000000000"
 vm_auth_cert_public="~/.ssh/id_rsa.pub"
 vm_auth_cert_private="~/.ssh_id_rsa"
 #prefix to use for the VM name for all nodes 
-vm_name_prefix="cb_"
+vm_name_prefix="rlec"
 #vnet name to keeps azure vms in the same subnet 
-vnet_name="cb-vnet1" 
+vnet_name="rlec-vnet1" 
 #azure service name for all nodes
-service_name="cbase-service"
+service_name="rlec-service"
 #region where to provision all nodes
 region="'west US'"
 #number of data-disks to attach - check the max data-disk allowed on each SKU
@@ -63,24 +63,24 @@ data_disk_size=1023
 
 
 ##jumpbox settings
-#disable jumpbox: set to 1 to diable jumpbox. jumpbox is provisioned for security reasons. you may need to open 8091 and other couchbase server ports to the public internet without it. 
+#disable jumpbox: set to 1 to diable jumpbox. jumpbox is provisioned for security reasons. you may need to open 8091 and other rlec server ports to the public internet without it. 
 disable_jumpbox=0
 #image to use for the jumpbox. using windows server by default
 jumpbox_image_name="a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-Datacenter-20160329-en.us-127GB.vhd"
 #jumpbox vm sku to use. 
 jumpbox_vm_sku="Standard_D2"
 #TODO: change this username
-jumpbox_vm_admin_account_name="cb_vmadmin"
+jumpbox_vm_admin_account_name="rl_vmadmin"
 #TODO: change this password
-jumpbox_vm_admin_account_password="couchB@SE123"
+jumpbox_vm_admin_account_password="redisl@bs123"
 
 ##cluster settings
 #ubuntu OS image to use on azure
-couchbase_vm_image_name="b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-14_04_4-LTS-amd64-server-20160314-en-us-30GB"
+rlec_vm_image_name="b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-14_04_4-LTS-amd64-server-20160314-en-us-30GB"
 #cluster vm sku to use. Standard_D2 can be used as the minimum HW. 
-couchbase_vm_sku="Standard_D2"
-#couchbase cluster vm admin account name
-couchbase_vm_admin_account_name="cb_vmadmin"
+rlec_vm_sku="Standard_D2"
+#rlec cluster vm admin account name
+rlec_vm_admin_account_name="rl_vmadmin"
 
 #misc settings
 #this will enable removing the .ssh/known_hosts file under MacOS. The file gets in the way of reprovisioning the same node names for the cluster.
